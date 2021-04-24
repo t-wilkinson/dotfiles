12543e96bc321beabc74c329cb93cf614ce253f2
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-04-24 17:48:59 -0400

    move link.sh into this repository

--- README.md
+++
@@ -1,1 +0,0 @@
-Look [here](https://github.com/t-wilkinson/scripts/blob/master/link.sh) for a script to link these files. Make sure change the $DOT variable.
---
+++ link.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Links all files in ~/dotfiles folder to their respective position
+
+DOT=$HOME/dev/t-wilkinson/dotfiles
+ZSH=${ZDOTDIR:-$HOME}
+
+mkdir -p $HOME/.xmonad/
+ln -sf $DOT/xmonad/xmonad.hs $HOME/.xmonad
+
+for f in $DOT/home/*; do
+    ln -sf $f "$HOME/.$(basename $f)"
+done
+
+mkdir -p $ZSH
+for f in $DOT/zsh/*; do
+    ln -sf $f "$ZSH/.$(basename $f)"
+    ln -sf $f "$ZSH/.zprezto/runcoms/$(basename $f)"
+done
+
+used=(zsh home xmonad)
+for d in $DOT/*; do
+    to="$HOME/.config/$(basename $d)"
+
+    if [[ -n $(grep $(basename $d) <<< $used) ]]; then
+        continue
+    fi
+
+    ln -sfn $d $to
+done
